20c14319d51ec5f44f6c2eef25c4ca49a1540996
jwoodsiii <woods3.jessie@gmail.com>
2026-01-29 18:00:23 -0800

    Adding site visit metrics

--- main.go
+++ main.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+
+	apiConfig := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	const filePathRoot = "."
+	const port = "8080"
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", apiConfig.middlewareMetricsInc(http.FileServer(http.Dir(filePathRoot)))))
+	// mux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+	// custom handler for readiness endpoint
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiConfig.handlerRequestCounter)
+	mux.HandleFunc("/reset", apiConfig.handlerReset)
+
 	server := http.Server{
-		Addr:    "localhost:8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
 	server.ListenAndServe()
 
+}
+
+func (cfg *apiConfig) handlerRequestCounter(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))))
 }
